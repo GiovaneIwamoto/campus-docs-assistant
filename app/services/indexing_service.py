f1aa7549fb9b88e18e607bffbef77626ef15bb7b
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-04-14 16:17:41 -0400

    feat: indexing service toast ui

--- app/services/indexing_service.py
+++ app/services/indexing_service.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from services.web_scraper import get_rendered_webpage
-from services.pinecone_service import initialize_pinecone
+from services.vectorstore_service import initialize_vectorstore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def run_indexing_mode(config: dict):
@@ -13,23 +13,29 @@ def run_indexing_mode(config: dict):
     embedding_model = config.get("embedding_model")
 
     try:
-        with st.spinner("Processing the web page content...", show_time=True):
+        with st.spinner("Processing web page and indexing...", show_time=True):
 
             # Initialize Pinecone
-            vector_store = initialize_pinecone(pinecone_api_key, pinecone_index_name, embedding_model)
+            vector_store = initialize_vectorstore(pinecone_api_key, pinecone_index_name, embedding_model)
+            st.toast('Pinecone initialized successfully!', icon=":material/table_eye:")
 
             # Load and chunk the web page content
             doc = get_rendered_webpage(web_url)
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
             all_splits = text_splitter.split_documents([doc])
+            st.toast('Web page content chunked successfully!', icon=":material/package:")
 
             # Display the number of chunks
             st.status(f"Number of chunks created: {len(all_splits)}",state="complete")
 
             # Index the chunks
             vector_store.add_documents(documents=all_splits)
+            st.toast('Chunks indexed successfully!', icon=":material/cloud_upload:")
 
         st.success(f"Web page content indexed successfully at Pinecone!", icon=":material/cloud_done:")
 
     except Exception as e:
-        st.error(f"An error occurred: {e}", icon=":material/error:")
+        st.error(f"An error occurred while indexing the web page.", icon=":material/cloud_off:")
+
+        with st.expander("Error details"):
+            st.exception(e)
