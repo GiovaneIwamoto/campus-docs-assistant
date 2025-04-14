b95df27e37b89d680ddbe0a63f7831f3f14bbf2d
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-04-14 16:16:23 -0400

    refactor: chat service variables and import

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -1,26 +1,29 @@
 import uuid
-import time
 import streamlit as st
 from langchain_core.messages import HumanMessage
 from langchain_community.chat_models.maritalk import MaritalkHTTPError
 from utils.chat_utils import format_chat_messages
-from utils.error_handling import handle_maritalk_error
+from utils.error_handler import handle_maritalk_error
 from config.logging_config import setup_logging
 from services.state_machine import app
 
 logger = setup_logging()
 
-def handle_user_input(prompt: str, api_key: str):
+def handle_user_input(prompt: str, llm_api_key: str):
     """
     Handle user input by appending it to the chat history, invoking the LLM, 
     and updating the session state with the response.
     """
-    if not api_key:
+    if not llm_api_key:
         st.info("Please add your LLM API key.", icon=":material/passkey:")
         return
 
     # Store the API key in session state
-    st.session_state["api_key"] = api_key
+    st.session_state["api_key"] = llm_api_key
+
+    # Initialize the chat history if not already present
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
 
     # Append the user's message to the chat history
     st.session_state["messages"].append(HumanMessage(content=prompt))
@@ -37,6 +40,6 @@ def handle_user_input(prompt: str, api_key: str):
         # Update the session state with the new chat history
         st.session_state["messages"] = output["messages"]
         logger.info(f"Chat history:\n{format_chat_messages(output['messages'])}\n")
-    
+
     except MaritalkHTTPError as e:
         handle_maritalk_error(e)
