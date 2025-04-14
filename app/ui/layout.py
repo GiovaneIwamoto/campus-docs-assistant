188cb4b1f77cd0ce0c76694f8ef1bb251b1ad7ba
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-04-14 16:23:23 -0400

    feat: ui set page config

--- app/ui/layout.py
+++ app/ui/layout.py
@@ -1,6 +1,20 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage
 
+def set_page_config():
+    # Set the page configuration for the Streamlit app
+    st.set_page_config(
+        page_title="Campus Docs Assistant",
+        page_icon=":material/collections_bookmark:",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'Get Help': 'https://github.com/GiovaneIwamoto/campus-docs-assistant/wiki',
+            'Report a bug': "https://github.com/GiovaneIwamoto/campus-docs-assistant/issues",
+            'About': ""
+        }
+    )
+
 def display_banner():
     st.image("assets/banner.png")
 
