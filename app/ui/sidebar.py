61929358c7428b2c7f930478baecb573b3a2dadf
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-04-15 00:18:53 -0400

    ui: sidebar with icons

--- app/ui/sidebar.py
+++ app/ui/sidebar.py
@@ -1,20 +1,19 @@
 import streamlit as st
 
 def configure_sidebar():
-    """
-    Configure the Streamlit sidebar for API key input and Indexing mode settings.
-    """
+    """"Configure the sidebar for the Streamlit app."""
+
     with st.sidebar:
         st.title("Settings")
         
         # API key llm input
-        llm_api_key = st.text_input("LLM API Key", type="password", value="")
+        llm_api_key = st.text_input("LLM API Key", type="password")
 
         # API key pinecone input
-        pinecone_api_key = st.text_input("Pinecone API Key", type="password", value="")
+        pinecone_api_key = st.text_input("Pinecone API Key", type="password")
         
         # Pinecone index name input
-        pinecone_index_name = st.text_input("Pinecone Index Name", "")
+        pinecone_index_name = st.text_input("Pinecone Index Name")
         
         # Embedding model input
         embedding_model = st.text_input("Embedding Model", value="nomic-embed-text")
@@ -25,18 +24,18 @@ def configure_sidebar():
         web_url = st.text_input("Enter the URL to scrape:")
         
         # Button to activate indexing mode
-        indexing_mode_enabled = st.button("Activate Indexing Mode")
+        indexing_mode_enabled = st.button("Activate Indexing", icon=":material/database_upload:")
 
     # Validate required fields
     if indexing_mode_enabled:
         if not web_url or not pinecone_api_key or not pinecone_index_name or not embedding_model:
             st.warning(
-                "**Indexing mode failed** — you must provide a valid URL and fill in all the required fields.",
-                icon=":material/warning:"
+                "**Indexing failed** — you must provide a valid URL and fill in all the required fields.",
+                icon=":material/assignment_late:"
             )
             indexing_mode_enabled = False
         else:
-            st.success("Indexing mode activated!",icon=":material/check_circle:")
+            st.success("Indexing activated!",icon=":material/check_circle:")
 
     indexing_mode_config = {
         "enabled": indexing_mode_enabled,
