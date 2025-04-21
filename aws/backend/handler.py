91cb412357aa9952db37df97670925ea85dace1e
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-04-21 18:10:09 -0300

    remove: old files

--- aws/backend/handler.py
+++
@@ -1,5 +0,0 @@
-from functions.telegramToLex import telegramToLex
-
-# Define functions for Serverless framework
-def telegramToLex_handler(event, context):
-    return telegramToLex(event, context)
