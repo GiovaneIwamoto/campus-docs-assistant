e7030da143c492177e1dfc28ef7d5c298188054f
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-04-23 00:01:30 -0300

    style: sidebar expanders

--- app/ui/sidebar.py
+++ app/ui/sidebar.py
@@ -4,14 +4,14 @@ def configure_sidebar() -> dict:
     """"Configure the sidebar for the Streamlit app."""
 
     with st.sidebar:
-        # Settings for LLM API key and Pinecone configuration
-        st.title("Settings")
-        
+        # Settings for LLM API key and Pinecone configuration        
+        keys_expander = st.expander("Settings", expanded=True)
+
         # Variables for LLM API key and Pinecone configuration
-        llm_api_key = st.text_input("Maritalk API Key", type="password")
-        pinecone_api_key = st.text_input("Pinecone API Key", type="password")
-        pinecone_index_name = st.text_input("Pinecone Index Name")
-        embedding_model = st.text_input("Ollama Embedding Model", value="nomic-embed-text")
+        llm_api_key = keys_expander.text_input("Maritalk API Key", type="password")
+        pinecone_api_key = keys_expander.text_input("Pinecone API Key", type="password")
+        pinecone_index_name = keys_expander.text_input("Pinecone Index Name")
+        embedding_model = keys_expander.text_input("Ollama Embedding Model", value="nomic-embed-text")
 
         # Initialization of session state variables
         if 'llm_api_key' not in st.session_state:
@@ -31,31 +31,30 @@ def configure_sidebar() -> dict:
         st.session_state['pinecone_api_key'] = pinecone_api_key  
         st.session_state['pinecone_index_name'] = pinecone_index_name
         st.session_state['embedding_model'] = embedding_model
-
+                
         # Settings for indexing mode
-        st.title("Indexing")
+        index_expander = st.expander("Indexing", expanded=True)
 
-        # Web URL input and button for activating indexing
-        web_url = st.text_input("Enter the URL to scrape:")
-        indexing_mode_enabled = st.button("Activate Indexing", icon=":material/database_upload:")
-        
+        # Web indexing section
+        web_url = index_expander.text_input("Web Link", placeholder="https://example.com")
+        web_indexing_enabled = index_expander.button("Activate Web Indexing", icon=":material/database_upload:")
+
         # File indexing section
-        st.markdown("---")
-        st.subheader("File Upload for Indexing")
-        uploaded_file = st.file_uploader("Upload a file (.pdf, .txt, .docx):", type=["pdf", "txt", "docx"])
-        file_indexing_enabled = st.button("Activate File Indexing", icon=":material/database_upload:")
+        uploaded_file = index_expander.file_uploader("File Upload", type=["pdf", "txt", "docx"])
+        file_indexing_enabled = index_expander.button("Activate File Indexing", icon=":material/database_upload:")
 
     # Validate required fields for web indexing
-    if indexing_mode_enabled:
+    if web_indexing_enabled:
         if not web_url or not pinecone_api_key or not pinecone_index_name or not embedding_model:
             st.toast(
-                "Indexing failed — you must provide a valid URL and fill in all the required fields.",
+                "Web Indexing failed — you must provide a valid URL and fill in all the required fields.",
                 icon=":material/assignment_late:"
             )
-            indexing_mode_enabled = False
+            web_indexing_enabled = False
         else:
-            st.toast("Indexing activated!",icon=":material/check_circle:")
+            st.toast("Web indexing activated!",icon=":material/check_circle:")
 
+    # Validate required fields for file indexing
     if file_indexing_enabled:
         if not uploaded_file or not pinecone_api_key or not pinecone_index_name or not embedding_model:
             st.toast(
@@ -64,10 +63,10 @@ def configure_sidebar() -> dict:
             )
             file_indexing_enabled = False
         else:
-            st.toast(f"File indexing activated for {uploaded_file.name}", icon=":material/check_circle:")
+            st.toast(f"File indexing activated!", icon=":material/check_circle:")
 
     indexing_mode_config = {
-        "enabled": indexing_mode_enabled,
+        "web_indexing_enabled": web_indexing_enabled,
         "web_url": web_url,
         "file_indexing_enabled": file_indexing_enabled,   
         "uploaded_file": uploaded_file,                   
