5a3441e2ba946a5dead2bf75145c8d18f9d34c3e
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-06-02 10:19:58 -0400

    deploy: sidebar secrets

--- app/ui/sidebar.py
+++ app/ui/sidebar.py
@@ -8,32 +8,35 @@ def configure_sidebar() -> dict:
         keys_expander = st.expander("Settings", expanded=True)
 
         # Variables for LLM API key and Pinecone configuration
-        llm_api_key = keys_expander.text_input("LLM API Key", type="password")
-        pinecone_api_key = keys_expander.text_input("Pinecone API Key", type="password")
-        pinecone_index_name = keys_expander.text_input("Pinecone Index Name")
+        llm_api_key = st.secrets["llm_api_key"]
+        pinecone_api_key = st.secrets["pinecone_api_key"]
+        pinecone_index_name = keys_expander.text_input("Pinecone Index Name", value=st.secrets["pinecone_index_name"], disabled=True)
         
         # Support for Ollama or OpenAI embedding models
-        embedding_model_provider = keys_expander.selectbox("Embedding Model Provider", ("OpenAI", "Ollama"), placeholder="Select an embedding model provider", index=None)
-        embedding_model = None
-        openai_api_key = None
-        previous_model_provider = st.session_state.get('previous_model_provider', None)
+        embedding_model_provider = st.secrets["embedding_model_provider"]
+        embedding_model = st.secrets["embedding_model"]
+        openai_api_key = st.secrets["openai_api_key"]
+        
+        # Disable this section for deployment, as keys are managed via Streamlit secrets
+
+        # previous_model_provider = st.session_state.get('previous_model_provider', None)
 
         # OpenAI as embedding model provider
-        if embedding_model_provider == "OpenAI":
-            embedding_model = keys_expander.selectbox("Embedding Model", ("text-embedding-3-small", "text-embedding-3-large"))
-            openai_api_key = keys_expander.text_input("OpenAI API Key", type="password")
+        # if embedding_model_provider == "OpenAI":
+        #     embedding_model = keys_expander.selectbox("Embedding Model", ("text-embedding-3-small", "text-embedding-3-large"))
+        #     openai_api_key = keys_expander.text_input("OpenAI API Key", type="password")
             
-            if previous_model_provider != "OpenAI":
-                st.toast("Using OpenAI's embedding model requires an OpenAI API key.", icon=":material/info:")
-                st.toast("Be sure to Pinecone's index is set up for the selected embedding model.", icon=":material/dataset_linked:")
+        #     if previous_model_provider != "OpenAI":
+        #         st.toast("Using OpenAI's embedding model requires an OpenAI API key.", icon=":material/info:")
+        #         st.toast("Be sure to Pinecone's index is set up for the selected embedding model.", icon=":material/dataset_linked:")
 
         # Ollama as embedding model provider
-        if embedding_model_provider == "Ollama":
-            embedding_model = keys_expander.text_input("Embedding Model", placeholder="nomic-embed-text")
+        # if embedding_model_provider == "Ollama":
+        #     embedding_model = keys_expander.text_input("Embedding Model", placeholder="nomic-embed-text")
             
-            if previous_model_provider != "Ollama":
-                st.toast("Using Ollama's embedding model requires Ollama to be installed and running.", icon=":material/info:")
-                st.toast("Be sure to Pinecone's index is set up for the selected embedding model.", icon=":material/dataset_linked:")
+        #     if previous_model_provider != "Ollama":
+        #         st.toast("Using Ollama's embedding model requires Ollama to be installed and running.", icon=":material/info:")
+        #         st.toast("Be sure to Pinecone's index is set up for the selected embedding model.", icon=":material/dataset_linked:")
 
         # Update session state variables   
         st.session_state['llm_api_key'] = llm_api_key
@@ -48,11 +51,11 @@ def configure_sidebar() -> dict:
         index_expander = st.expander("Indexing", expanded=True)
 
         # Web indexing section
-        web_url = index_expander.text_input("Web Link", placeholder="https://example.com")
+        web_url = index_expander.text_input("Web Link", placeholder="https://example.com", disabled=True)
         web_indexing_enabled = index_expander.button("Activate Web Indexing", icon=":material/database_upload:")
 
         # File indexing section
-        uploaded_files = index_expander.file_uploader("File Upload", type=["pdf", "txt", "docx", "zip"], accept_multiple_files=True)
+        uploaded_files = index_expander.file_uploader("File Upload", type=["pdf", "txt", "docx", "zip"], accept_multiple_files=True, disabled=True)
         file_indexing_enabled = index_expander.button("Activate File Indexing", icon=":material/database_upload:")
 
     # Validate required fields for web indexing
